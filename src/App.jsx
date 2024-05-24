76a0150c26b9a5239fccc02302eec03a46e791f8
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-05-24 13:17:03 +0700

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
+import Body from "./components/Body_HomePage.jsx";
 
 function App() {
   //vertical center
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
+    <div className="overflow-x-hidden">
+      <Header className="mb-20" />
+      <Body className="my-20" />{" "}
+      {/* Add margin-top and margin-bottom to the Appointment */}
+      <Footer className="mt-10" /> {/* Add margin-top to the Footer */}
+    </div>
   );
 }
 
---
+++ src/components/Body_HomePage.jsx
@@ -0,0 +1,16 @@
+//import {Appointment} from "./Appointment.jsx";
+import diamond from "../assets/images/diamond_poster.png";
+
+export default function Body({ className }) {
+  return (
+    <div
+      className={`relative w-[1440px] h-[560px] bg-[#717171] rounded-[4px] ${className}`}
+    >
+      <img
+        className="absolute w-[681px] h-[555px] top-[5px] left-[753px]"
+        alt="Image"
+        src={diamond}
+      />
+    </div>
+  );
+}
