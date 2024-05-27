224392b9c996ca31051d74884724a3d530863a0e
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-05-27 15:52:22 +0700

    updated root layout component

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,17 +1,13 @@
+import { Outlet } from "react-router-dom";
 import Header from "./components/common/Header.jsx";
 import Footer from "./components/common/Footer.jsx";
-import Body_HomePage from "./components/home_page/HomepageImage.jsx";
-import Supplier from "./components/home_page/Supplier.jsx";
 
 function RootLayout() {
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <Header />
       <div style={{ margin: "50px 0" }}>
-        <Body_HomePage />
-      </div>
-      <div style={{ margin: "50px 0" }}>
-        <Supplier />
+        <Outlet />
       </div>
       <Footer />
     </div>
