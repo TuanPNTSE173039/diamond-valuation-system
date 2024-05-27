73618ac189e7b99dfc1e180ef042cdbe4b319448
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-05-27 15:49:12 +0700

    added result calculation of diamond

---
+++ src/components/calculate/CalculateOutput.jsx
@@ -0,0 +1,202 @@
+export const CalculateOutput = () => {
+  return (
+    <div className="relative w-[743px] h-[604px] right-[60px]">
+      <div className="w-[747px] h-[604px] top-0 left-0">
+        <div className="absolute [font-family:'Epilogue-Bold',Helvetica] font-bold text-[#171a1f] text-[20px] tracking-[0] leading-[30px] whitespace-nowrap">
+          Calculator Output
+        </div>
+        <div className="w-[740px] h-[86px] top-[402px] left-[3px] bg-white absolute rounded-[4px] shadow-[0px_0px_1px_#171a1f12,0px_0px_2px_#171a1f1f]">
+          <div className="absolute w-[28px] h-[32px] top-[25px] left-[352px]">
+            <div className="w-[9px] h-[10px] top-[3px] left-[10px] bg-white absolute rounded-[4px] shadow-[0px_0px_1px_#171a1f12,0px_0px_2px_#171a1f1f]" />
+
+            <div className="top-[18px] absolute left-0 [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[8px] tracking-[0] leading-[14px] whitespace-nowrap">
+              COLOR
+            </div>
+            <div className="absolute top-0 left-[11px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[14px] text-center tracking-[0] leading-[22px] whitespace-nowrap">
+              G
+            </div>
+          </div>
+          <div className="absolute w-[96px] h-[27px] top-[29px] left-[446px]">
+            <div className="top-0 left-0 text-[#9095a0] text-[12px] leading-[20px] absolute [font-family:'Inter-Regular',Helvetica] font-normal tracking-[0] whitespace-nowrap">
+              DANIEL WILLIAM
+            </div>
+            <div className="top-[13px] left-[51px] text-[#bcc1ca] text-[8px] leading-[14px] absolute [font-family:'Inter-Regular',Helvetica] font-normal tracking-[0] whitespace-nowrap">
+              0
+            </div>
+            <div className="top-[13px] left-[42px] text-[#bcc1ca] text-[8px] leading-[14px] absolute [font-family:'Inter-Regular',Helvetica] font-normal tracking-[0] whitespace-nowrap">
+              M
+            </div>
+            <div className="top-[13px] left-[69px] text-[#bcc1ca] text-[8px] leading-[14px] absolute [font-family:'Inter-Regular',Helvetica] font-normal tracking-[0] whitespace-nowrap">
+              D
+            </div>
+            <div className="absolute top-[13px] left-[19px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#bcc1ca] text-[8px] tracking-[0] leading-[14px] whitespace-nowrap">
+              D
+            </div>
+            <div className="top-[13px] left-[60px] text-[#bcc1ca] text-[8px] leading-[14px] absolute [font-family:'Inter-Regular',Helvetica] font-normal tracking-[0] whitespace-nowrap">
+              N
+            </div>
+            <div className="top-[13px] left-[34px] text-[#bcc1ca] text-[8px] leading-[14px] absolute [font-family:'Inter-Regular',Helvetica] font-normal tracking-[0] whitespace-nowrap">
+              A
+            </div>
+          </div>
+          <div className="top-[31px] left-[620px] [font-family:'Inter-Regular',Helvetica] font-normal text-[12px] leading-[20px] absolute text-[#171a1f] tracking-[0] whitespace-nowrap">
+            $2,490
+          </div>
+          <div className="absolute w-[47px] h-[34px] top-[23px] left-[107px]">
+            <div className="absolute top-[20px] left-0 [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[8px] tracking-[0] leading-[14px] whitespace-nowrap">
+              CUT SCORE
+            </div>
+            <div className="top-0 left-[16px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#6a63ee] text-[14px] leading-[22px] absolute tracking-[0] whitespace-nowrap">
+              8.9
+            </div>
+          </div>
+          <div className="absolute w-[39px] h-[35px] top-[23px] left-[275px]">
+            <div className="top-[21px] left-0 absolute [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[9px] tracking-[0] leading-[14px] whitespace-nowrap">
+              CLARITY
+            </div>
+            <div className="top-0 left-[7px] absolute [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[13px] tracking-[0] leading-[22px] whitespace-nowrap">
+              VS2
+            </div>
+          </div>
+          <div className="top-[23px] left-[208px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[12px] leading-[20px] absolute tracking-[0] whitespace-nowrap">
+            0.85
+          </div>
+          <div className="top-[43px] left-[207px] absolute [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[8px] tracking-[0] leading-[14px] whitespace-nowrap">
+            CARAT
+          </div>
+          <img
+            className="absolute w-[16px] h-[16px] top-[35px] left-[710px]"
+            alt="Caret right"
+            src="caret-right-1.svg"
+          />
+          <img
+            className="absolute w-[70px] h-[58px] top-[15px] left-[7px]"
+            alt="Image"
+            src="image-140.png"
+          />
+        </div>
+        <div className="w-[740px] h-[86px] top-[518px] left-[3px] bg-gray-50 absolute rounded-[4px] shadow-[0px_0px_1px_#171a1f12,0px_0px_2px_#171a1f1f]">
+          <div className="absolute w-[161px] h-[62px] top-[12px] left-[411px]">
+            <div className="absolute top-0 left-[89px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#565e6c] text-[10px] tracking-[0] leading-[16px] whitespace-nowrap">
+              DREAMSTONE.
+            </div>
+            <div className="absolute w-[110px] h-[59px] top-[3px] left-0">
+              <div className="absolute top-[19px] left-0 [font-family:'Inter-Regular',Helvetica] font-normal text-[#9095a0] text-[10px] tracking-[0] leading-[16px] whitespace-nowrap">
+                DANIEL WILLIAM
+              </div>
+              <div className="absolute top-[41px] left-[57px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#aba7de] text-[11px] tracking-[0] leading-[18px] whitespace-nowrap">
+                ROCKHER
+              </div>
+              <div className="absolute top-0 left-[17px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#bcc1ca] text-[14px] tracking-[0] leading-[22px] whitespace-nowrap">
+                Adiamor
+              </div>
+              <div className="absolute top-[31px] left-[15px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#bcc1ca] text-[8px] tracking-[0] leading-[14px] whitespace-nowrap">
+                DIAMO
+              </div>
+              <div className="absolute top-[32px] left-[56px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#bcc1ca] text-[7px] tracking-[0] leading-[12px] whitespace-nowrap">
+                DS
+              </div>
+            </div>
+          </div>
+          <div className="absolute w-[28px] h-[33px] top-[24px] left-[352px]">
+            <div className="w-[9px] h-[10px] top-[4px] left-[10px] bg-gray-50 absolute rounded-[4px] shadow-[0px_0px_1px_#171a1f12,0px_0px_2px_#171a1f1f]" />
+            <div className="top-[19px] absolute left-0 [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[8px] tracking-[0] leading-[14px] whitespace-nowrap">
+              COLOR
+            </div>
+            <div className="absolute top-0 left-[11px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[14px] text-center tracking-[0] leading-[22px] whitespace-nowrap">
+              G
+            </div>
+          </div>
+          <div className="top-[30px] left-[620px] [font-family:'Inter-Regular',Helvetica] font-normal text-[12px] leading-[20px] absolute text-[#171a1f] tracking-[0] whitespace-nowrap">
+            $2,958
+          </div>
+          <div className="absolute w-[47px] h-[35px] top-[22px] left-[107px]">
+            <div className="absolute top-[21px] left-0 [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[8px] tracking-[0] leading-[14px] whitespace-nowrap">
+              CUT SCORE
+            </div>
+            <div className="top-0 left-[16px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#6a63ee] text-[14px] leading-[22px] absolute tracking-[0] whitespace-nowrap">
+              9.6
+            </div>
+          </div>
+          <div className="top-[44px] left-[275px] absolute [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[9px] tracking-[0] leading-[14px] whitespace-nowrap">
+            CLARITY
+          </div>
+          <div className="top-[22px] left-[282px] absolute [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[13px] tracking-[0] leading-[22px] whitespace-nowrap">
+            VS2
+          </div>
+          <div className="absolute w-[27px] h-[35px] top-[22px] left-[207px]">
+            <div className="top-[21px] left-0 absolute [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[8px] tracking-[0] leading-[14px] whitespace-nowrap">
+              CARAT
+            </div>
+            <div className="top-0 left-[5px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[14px] leading-[22px] absolute tracking-[0] whitespace-nowrap">
+              0.9
+            </div>
+          </div>
+          <img
+            className="absolute w-[16px] h-[16px] top-[35px] left-[710px]"
+            alt="Right arrow"
+            src="right-arrow-1.svg"
+          />
+          <img
+            className="absolute w-[70px] h-[58px] top-[14px] left-[7px]"
+            alt="Image"
+            src="image-138.png"
+          />
+        </div>
+        <div className="absolute top-[496px] left-[5px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#9095a0] text-[10px] tracking-[0] leading-[16px] whitespace-nowrap">
+          Top Diamonds
+        </div>
+        <div className="absolute top-[381px] left-[5px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#9095a0] text-[10px] tracking-[0] leading-[16px] whitespace-nowrap">
+          Sponsored
+        </div>
+        <img
+          className="absolute w-[14px] h-[12px] top-[374px] left-[61px]"
+          alt="Image"
+          src="image-139.png"
+        />
+        <div className="absolute w-[740px] h-[307px] top-[32px] left-[3px]">
+          <div className="w-[740px] h-[205px] top-0 left-0 bg-white absolute rounded-[4px] shadow-[0px_0px_1px_#171a1f12,0px_0px_2px_#171a1f1f]">
+            <div className="top-[63px] left-[283px] [font-family:'Epilogue-Bold',Helvetica] font-bold text-[57px] leading-[74px] absolute text-[#171a1f] tracking-[0] whitespace-nowrap">
+              $5,094
+            </div>
+            <div className="absolute top-[30px] left-[283px] [font-family:'Epilogue-Bold',Helvetica] font-bold text-[#171a1f] text-[18px] tracking-[0] leading-[28px] whitespace-nowrap">
+              Fair Price Estimate
+            </div>
+            <p className="absolute top-[146px] left-[310px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#9095a0] text-[12px] tracking-[0] leading-[20px] whitespace-nowrap">
+              Round 1 Carat G VS2
+            </p>
+            <button className="all-[unset] box-border absolute w-[98px] h-[20px] top-[170px] left-[321px] bg-green-50 rounded-[3px] overflow-hidden">
+              <div className="absolute top-0 left-[6px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#39945b] text-[11px] tracking-[0] leading-[18px] whitespace-nowrap">
+                Natural Diamond
+              </div>
+            </button>
+          </div>
+          <div className="w-[242px] h-[103px] top-[204px] left-[498px] bg-gray-50 absolute rounded-[4px] shadow-[0px_0px_1px_#171a1f12,0px_0px_2px_#171a1f1f]">
+            <div className="absolute top-[24px] left-[48px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#9095a0] text-[12px] tracking-[0] leading-[20px] whitespace-nowrap">
+              Estimate Price per Carat
+            </div>
+            <div className="top-[52px] left-[100px] [font-family:'Inter-Regular',Helvetica] font-normal text-[16px] leading-[26px] absolute text-[#171a1f] tracking-[0] whitespace-nowrap">
+              $5,094
+            </div>
+          </div>
+          <div className="w-[254px] h-[103px] top-[204px] left-[243px] bg-gray-50 absolute rounded-[4px] shadow-[0px_0px_1px_#171a1f12,0px_0px_2px_#171a1f1f]">
+            <div className="absolute top-[23px] left-[61px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#9095a0] text-[12px] tracking-[0] leading-[20px] whitespace-nowrap">
+              Last 30 Day Change
+            </div>
+            <div className="top-[53px] left-[98px] [font-family:'Epilogue-Bold',Helvetica] font-bold text-[#171a1f] text-[19px] leading-[30px] absolute tracking-[0] whitespace-nowrap">
+              +7.59%
+            </div>
+          </div>
+          <div className="w-[242px] h-[103px] top-[204px] left-0 bg-gray-50 absolute rounded-[4px] shadow-[0px_0px_1px_#171a1f12,0px_0px_2px_#171a1f1f]">
+            <div className="top-[53px] left-[57px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#171a1f] text-[15px] text-center leading-[24px] absolute tracking-[0] whitespace-nowrap">
+              $4,065 - $6,226
+            </div>
+            <div className="absolute top-[24px] left-[61px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#9095a0] text-[12px] tracking-[0] leading-[20px] whitespace-nowrap">
+              Estimate Range
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
