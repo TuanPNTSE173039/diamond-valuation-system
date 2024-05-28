1aacefba6f68c641f55c8293c7cb206b15192843
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-05-28 21:26:36 +0700

    added diamond check input screen

---
+++ src/screens/DiamondCheck/CheckInput.jsx
@@ -0,0 +1,7 @@
+import CheckInputPage from "../../components/DiamondCheck/CheckInput/Page.jsx";
+
+const ScreenDiamondCheckInput = () => {
+    return <CheckInputPage />;
+}
+
+export default ScreenDiamondCheckInput;
