f87d1a8f3fcf5c01cb01046c21fac9e05d4d579f
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-05-29 23:40:02 +0700

    added breadcrumb component to root layout

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,17 +1,19 @@
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Header from "./UI/Header.jsx";
 import Footer from "./UI/Footer.jsx";
+import CustomSeparator from "./UI/BreadCrumb.jsx";
 
 function RootLayout() {
-  return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <Header />
-      <div style={{ margin: "50px 0" }}>
-        <Outlet />
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <Header/>
+            <CustomSeparator/>
+            <div style={{margin: "50px 0"}}>
+                <Outlet/>
+            </div>
+            <Footer/>
+        </div>
+    );
 }
 
 export default RootLayout;
