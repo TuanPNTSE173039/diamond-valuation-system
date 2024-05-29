cc2af0e32e57d6b59c92e18a123627c5ae46bd9d
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-05-29 17:37:54 +0700

    added origin button component

--- src/components/Calculate/InputForm/Button/Origin.jsx
+++ src/components/Calculate/InputForm/Button/Origin.jsx
@@ -1,37 +1,36 @@
-import {Box, Button} from "@mui/material";
+import { Box, Button } from '@mui/material';
 
 // eslint-disable-next-line react/prop-types
-const OriginButton = ({onClick, buttonName, icon, state, children}) => {
+const OriginButton = ({ onClick, buttonName, icon, state, children, setState }) => {
     return (
         <Button
-            onClick={onClick()}
+            onClick={() => onClick(setState, buttonName)}
             sx={{
-                width: "45%",
+                width: '45%',
                 height: 37,
-                position: "absolute",
+                position: 'absolute',
                 top: 85,
-                backgroundColor:
-                    {state}[{buttonName}] === "clicked" ? "#4F46E5" : "#f5f5f5",
-                ":hover": {bgcolor: "#4F46E5", color: "white"},
-                fontFamily: "Inter-Regular, Helvetica",
-                color:
-                    {state}[{buttonName}] === "clicked" ? "white" : "#171a1f",
+                backgroundColor: state[buttonName] === 'clicked' ? '#4F46E5' : '#f5f5f5',
+                ':hover': { bgcolor: '#4F46E5', color: 'white' },
+                fontFamily: 'Inter-Regular, Helvetica',
+                color: state[buttonName] === 'clicked' ? 'white' : '#171a1f',
                 fontSize: 12,
             }}
         >
-            <Box sx={{
-                position: "absolute",
-                width: "5%",
-                height: "20%",
-                top: 8,
-                left: 10,
-            }}>
+            <Box
+                sx={{
+                    position: 'absolute',
+                    width: '5%',
+                    height: '20%',
+                    top: 8,
+                    left: 10,
+                }}
+            >
                 {icon}
             </Box>
-
             {children}
         </Button>
     );
-}
+};
 
 export default OriginButton;
