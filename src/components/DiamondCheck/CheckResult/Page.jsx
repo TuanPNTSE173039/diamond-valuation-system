bb8ad26b7fbb3bca950478e59bc91a4caa9978b9
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-05-29 23:39:08 +0700

    added image list to diamond check result page

--- src/components/DiamondCheck/CheckResult/Page.jsx
+++ src/components/DiamondCheck/CheckResult/Page.jsx
@@ -1,11 +1,40 @@
 import diamond_id from "../../../assets/images/diamond_id.png";
-import Form from "./Form.jsx";
+import daniel from "../../../assets/images/daniel.png";
+import allurez from "../../../assets/images/allurez.png";
+import Form from "./ResultForm.jsx";
+import ImageGallery from '../CheckResult/ImageForm';
+import {Box} from "@mui/material";
+
 
 const DiamondCheckResultPage = () => {
+
+
+  const images = [
+    {
+      img: diamond_id,
+      title: 'Diamond ID Image' // replace with your actual title
+    },
+    {
+      img: diamond_id,
+      title: 'Diamond ID Image' // replace with your actual title
+    },
+    {
+      img: diamond_id,
+      title: 'Diamond ID Image' // replace with your actual title
+    },
+    {
+      img: daniel,
+      title: 'Daniel Image' // replace with your actual title
+    },
+    {
+      img: allurez,
+      title: 'Allurez Image' // replace with your actual title
+    }
+  ];
   const giaId = "2474506136";
   const priceEstimate = "$7,117";
   const estimateRange = "$5,489 - $8,882";
-  const imageSrc = diamond_id;
+  //const imageSrc = diamond_id;
   const cut_score = "9.4";
   const visual_carat = "1.01";
   const shape = "Round";
@@ -18,24 +47,31 @@ const DiamondCheckResultPage = () => {
   const lab = "GIA";
 
   return (
-    <div>
-      <Form
-        giaId={giaId}
-        priceEstimate={priceEstimate}
-        estimateRange={estimateRange}
-        imageSrc={imageSrc}
-        cut_score={cut_score}
-        visual_carat={visual_carat}
-        shape={shape}
-        carat={carat}
-        color={color}
-        clarity={clarity}
-        fluorescence={fluorescence}
-        symmetry={symmetry}
-        Polish={Polish}
-        lab={lab}
-      />
-    </div>
+      <div className="relative w-[1440px] h-[626px] bg-white rounded-[4px] flex ">
+        <Box
+          width= "50%"
+          marginLeft="120px"
+        >
+          <ImageGallery images={images}/>
+        </Box>
+        <div style={{width: '50%'}}>
+          <Form
+              giaId={giaId}
+              priceEstimate={priceEstimate}
+              estimateRange={estimateRange}
+              cut_score={cut_score}
+              visual_carat={visual_carat}
+              shape={shape}
+              carat={carat}
+              color={color}
+              clarity={clarity}
+              fluorescence={fluorescence}
+              symmetry={symmetry}
+              Polish={Polish}
+              lab={lab}
+          />
+        </div>
+      </div>
   );
 };
 
