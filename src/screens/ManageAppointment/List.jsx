03e6ed72c5e0aebf9c5c214d9cf37e07a62d9531
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-05-30 23:37:27 +0700

    added manage account component

---
+++ src/screens/ManageAppointment/List.jsx
@@ -0,0 +1,8 @@
+import ValuationRequestList from "../../components/ManageAccount/ManageAppointment/List.jsx";
+
+
+const ScreenValuationRequestList = () => {
+    return <ValuationRequestList />;
+};
+
+export default ScreenValuationRequestList;
---
+++ src/utilities/Status.js
@@ -0,0 +1,16 @@
+const valuationRequestStatus = [
+    {
+        id: 0,
+        name: "All requests",
+    },
+    {
+        id: 1,
+        name: "Pending",
+    },
+    {
+        id: 2,
+        name: "Processing",
+    },
+];
+
+export default valuationRequestStatus;
