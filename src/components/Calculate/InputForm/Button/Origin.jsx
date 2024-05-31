cc4528836327ada9026c45d8300b2156e2a1c48a
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-05-31 21:26:26 +0700

    updated origin button

--- src/components/Calculate/InputForm/Button/Origin.jsx
+++ src/components/Calculate/InputForm/Button/Origin.jsx
@@ -10,6 +10,8 @@ const OriginButton = ({ onClick, buttonName, icon, state, children, setState }) 
                 height: 37,
                 position: 'absolute',
                 top: 85,
+                boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                borderRadius: '6px',
                 backgroundColor: state[buttonName] === 'clicked' ? '#4F46E5' : '#f5f5f5',
                 ':hover': { bgcolor: '#4F46E5', color: 'white' },
                 fontFamily: 'Inter-Regular, Helvetica',
