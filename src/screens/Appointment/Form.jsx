b9da428f6e50f8759334cf70ab6725903f3b4005
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-01 19:50:05 +0700

    updated

--- src/screens/Appointment/Form.jsx
+++ src/screens/Appointment/Form.jsx
@@ -2,18 +2,38 @@ import AppointmentForm from "../../components/Appointment/Form.jsx";
 import { useQuery } from "@tanstack/react-query";
 import { getServices } from "../../services/Service/api.js";
 import UICircularIndeterminate from "../../components/UI/CircularIndeterminate.jsx";
+import { getCustomer, getCustomers } from "../../services/Customers/api.js";
+import { getCustomerByID } from "../../services/Customers/utils.js";
 
 const ScreenAppointmentForm = () => {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["Services"],
+  const {
+    data: services,
+    isLoading: isServiceLoading,
+    error: serviceError,
+  } = useQuery({
+    queryKey: ["services"],
     queryFn: getServices,
   });
-  if (isLoading) {
+
+  const {
+    data: customer,
+    isLoading: isCustomerLoading,
+    error: customerError,
+  } = useQuery({
+    queryKey: ["customer"],
+    queryFn: () => getCustomer(1),
+  });
+
+  if (isServiceLoading || isCustomerLoading) {
     return <UICircularIndeterminate />;
   }
-  console.log(data);
 
-  return <AppointmentForm services={getServices} />;
+  // const customer = getCustomerByID(customers, 1);
+
+  console.log("Services: ", services);
+  console.log("Customer: ", customer);
+
+  return <AppointmentForm services={services} customer={customer} />;
 };
 
 export default ScreenAppointmentForm;
--- src/services/Service/api.js
+++ src/services/Service/api.js
@@ -1,6 +1,7 @@
 import { http } from "../config.js";
+import axios from "axios";
 
 export const getServices = async () => {
-    const response = await http.get("services");
-    return response.data;
+  const response = await http.get("services");
+  return response.data;
 };
