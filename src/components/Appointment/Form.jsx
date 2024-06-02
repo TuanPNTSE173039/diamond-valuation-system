a0935e950517c443b535b145b7b1d809239a66fc
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-02 13:09:59 +0700

    updated

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -75,277 +75,288 @@ const AppointmentForm = ({ services, customer }) => {
     setAppointmentTime(appointmentTime);
   };
 
-  return (
-    <Box
-      sx={{
-        position: "relative",
-        width: "100%",
-        height: "560px",
-        bgcolor: "#717171",
-        marginTop: "20px",
-        marginBottom: "30px",
-      }}
-    >
-      <Box
-        component="img"
-        src={diamond}
-        alt="Diamond"
-        sx={{
-          position: "absolute",
-          width: "681px",
-          height: "555px",
-          top: "5px",
-          left: "740px",
-        }}
-      />
+    return (
       <Box
         sx={{
-          position: "absolute",
-          width: "490px",
+          position: "relative",
+          width: "100%",
           height: "560px",
-          top: "0px",
-          left: "204px",
-          bgcolor: "#d8c0989e",
-          p: 3,
+          bgcolor: "#717171",
+          marginTop: "20px",
+          marginBottom: "30px",
         }}
       >
-        <Typography
-          variant="h5"
-          component="div"
+        <Box
+          component="img"
+          src={diamond}
+          alt="Diamond"
           sx={{
-            fontFamily: "'Epilogue-Bold', Helvetica",
-            fontWeight: "bold",
-            color: "#003565",
-            mb: 2,
-            ml: "22px",
+            position: "absolute",
+            width: "681px",
+            height: "555px",
+            top: "5px",
+            left: "740px",
+          }}
+        />
+        <Box
+          sx={{
+            position: "absolute",
+            width: "490px",
+            height: "560px",
+            top: "0px",
+            left: "204px",
+            bgcolor: "#d8c0989e",
+            p: 3,
           }}
         >
-          Appointments
-        </Typography>
-
-        <form>
-          <FormControl sx={{ width: "400px", ml: "22px" }}>
-            <FormLabel sx={{ color: "white", margin: "2px" }}>Name*</FormLabel>
-            <TextField
-              variant="outlined"
-              required
-              placeholder="Input your name"
-              value={customer.lastName + " " + customer.firstName}
-              disabled
-              InputProps={{
-                sx: {
-                  height: "40px",
-                  backgroundColor: "white",
-                  color: "black",
-                },
-              }}
-            />
-          </FormControl>
-          <FormControl sx={{ width: "400px", ml: "22px" }}>
-            <FormLabel sx={{ color: "white", margin: "2px" }}>
-              Identity Card*
-            </FormLabel>
-            <TextField
-              variant="outlined"
-              required
-              placeholder="Input identity card"
-              value={customer.identityDocument}
-              disabled
-              InputProps={{
-                sx: {
-                  height: "40px",
-                  backgroundColor: "white",
-                },
-              }}
-            />
-          </FormControl>
-
-          <FormControl sx={{ width: "400px", ml: "22px" }}>
-            <FormLabel sx={{ color: "white", margin: "2px" }}>Phone*</FormLabel>
-            <TextField
-              variant="outlined"
-              required
-              placeholder="Input your phone"
-              value={customer.phone}
-              disabled
-              InputProps={{
-                sx: {
-                  height: "40px",
-                  backgroundColor: "white",
-                },
-              }}
-            />
-          </FormControl>
-
-          <FormControl sx={{ width: "400px", ml: "22px" }}>
-            <FormLabel sx={{ color: "white", margin: "2px" }}>
-              Address*
-            </FormLabel>
-            <TextField
-              variant="outlined"
-              required
-              placeholder="Input address"
-              value={customer.address}
-              disabled
-              InputProps={{
-                sx: {
-                  height: "40px",
-                  backgroundColor: "white",
-                },
-              }}
-            />
-          </FormControl>
-
-          <FormControl sx={{ width: "400px", ml: "22px" }}>
-            <FormLabel sx={{ color: "white", margin: "2px" }}>
-              Service*
-            </FormLabel>
-            <Select
-              variant="outlined"
-              required
-              sx={{
-                color: service === "Choose service" ? " #a9a9a9" : "black",
-                height: "40px",
-                backgroundColor: "white",
-                fontFamily: "'Your Font Family', sans-serif",
-              }}
-              value={service}
-              onChange={handleServiceChange}
-            >
-              <MenuItem value="Choose service" disabled>
-                Choose service
-              </MenuItem>
-              {services.map((service, index) => (
-                <MenuItem key={index} value={service.name}>
-                  {service.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-
-          <FormControl sx={{ width: "400px", ml: "22px" }}>
-            <FormLabel sx={{ color: "white", margin: "2px" }}>
-              Quantity*
-            </FormLabel>
-            <TextField
-              type="number"
-              variant="outlined"
-              required
-              placeholder="Input quantity"
-              value={diamondQuantity || ""}
-              onChange={handleDiamondQuantityChange}
-              InputProps={{
-                sx: {
-                  height: "40px",
-                  backgroundColor: "white",
-                },
-              }}
-              inputProps={{
-                min: 0,
-              }}
-            />
-          </FormControl>
-
-          <Button
-            onClick={handleSubmit}
-            variant="contained"
-            color="primary"
-            fullWidth
-            sx={{ mt: 2, width: "400px", ml: "22px" }}
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{
+              fontFamily: "'Epilogue-Bold', Helvetica",
+              fontWeight: "bold",
+              color: "#003565",
+              mb: 2,
+              ml: "22px",
+            }}
           >
-            Submit
-          </Button>
-        </form>
-        <Dialog
-          open={open}
-          onClose={() => setOpen(false)}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">
-            <Box display="flex" justifyContent="center" alignItems="center">
-              <CheckCircleOutlineIcon
-                color="success"
-                style={{ fontSize: 70 }}
+            Appointments
+          </Typography>
+
+          <form>
+            <FormControl sx={{ width: "400px", ml: "22px" }}>
+              <FormLabel sx={{ color: "white", margin: "2px" }}>
+                Name*
+              </FormLabel>
+              <TextField
+                variant="outlined"
+                required
+                placeholder="Input your name"
+                value={customer.lastName + " " + customer.firstName}
+                disabled
+                InputProps={{
+                  sx: {
+                    height: "40px",
+                    backgroundColor: "white",
+                    color: "black",
+                  },
+                }}
               />
-            </Box>
-            <Typography
-              align="center"
-              style={{
-                fontWeight: "bold",
-                color: "green",
-                fontSize: 20,
-              }}
-            >
-              {"Successful Appointment"}
-            </Typography>
-          </DialogTitle>
-          <DialogContent>
-            <Box id="alert-dialog-description">
-              <Box
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                align="center"
-                marginBottom="10px"
+            </FormControl>
+            <FormControl sx={{ width: "400px", ml: "22px" }}>
+              <FormLabel sx={{ color: "white", margin: "2px" }}>
+                Identity Card*
+              </FormLabel>
+              <TextField
+                variant="outlined"
+                required
+                placeholder="Input identity card"
+                value={customer.identityDocument}
+                disabled
+                InputProps={{
+                  sx: {
+                    height: "40px",
+                    backgroundColor: "white",
+                  },
+                }}
+              />
+            </FormControl>
+
+            <FormControl sx={{ width: "400px", ml: "22px" }}>
+              <FormLabel sx={{ color: "white", margin: "2px" }}>
+                Phone*
+              </FormLabel>
+              <TextField
+                variant="outlined"
+                required
+                placeholder="Input your phone"
+                value={customer.phone}
+                disabled
+                InputProps={{
+                  sx: {
+                    height: "40px",
+                    backgroundColor: "white",
+                  },
+                }}
+              />
+            </FormControl>
+
+            <FormControl sx={{ width: "400px", ml: "22px" }}>
+              <FormLabel sx={{ color: "white", margin: "2px" }}>
+                Address*
+              </FormLabel>
+              <TextField
+                variant="outlined"
+                required
+                placeholder="Input address"
+                value={customer.address}
+                disabled
+                InputProps={{
+                  sx: {
+                    height: "40px",
+                    backgroundColor: "white",
+                  },
+                }}
+              />
+            </FormControl>
+
+            <FormControl sx={{ width: "400px", ml: "22px" }}>
+              <FormLabel sx={{ color: "white", margin: "2px" }}>
+                Service*
+              </FormLabel>
+              <Select
+                variant="outlined"
+                required
+                sx={{
+                  color: service === "Choose service" ? " #a9a9a9" : "black",
+                  height: "40px",
+                  backgroundColor: "white",
+                  fontFamily: "'Your Font Family', sans-serif",
+                }}
+                value={service} // Use the state variable as the value
+                onChange={handleServiceChange} // Update the state when a service is selected
               >
-                <Typography> Appointment details:</Typography>
-                <Typography style={{ fontWeight: "bold", color: "#AE9A7F" }}>
-                  Code: #{valuationRequestId}
-                </Typography>
+                <MenuItem value="Choose service" disabled>
+                  Choose service
+                </MenuItem>
+                {services.map((service, index) => (
+                  <MenuItem key={index} value={service.name}>
+                    {service.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <FormControl sx={{ width: "400px", ml: "22px" }}>
+              <FormLabel sx={{ color: "white", margin: "2px" }}>
+                Quantity*
+              </FormLabel>
+              <TextField
+                type="number"
+                variant="outlined"
+                required
+                placeholder="Input quantity"
+                value={diamondQuantity || ""}
+                onChange={handleDiamondQuantityChange}
+                InputProps={{
+                  sx: {
+                    height: "40px",
+                    backgroundColor: "white",
+                  },
+                }}
+                inputProps={{
+                  min: 0,
+                }}
+              />
+            </FormControl>
+
+            <Button
+              onClick={handleSubmit}
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{ mt: 2, width: "400px", ml: "22px" }}
+            >
+              Submit
+            </Button>
+          </form>
+
+          <Dialog
+            open={open}
+            onClose={() => setOpen(false)}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <CheckCircleOutlineIcon
+                  color="success"
+                  style={{ fontSize: 70 }}
+                />
               </Box>
-              <Box display="flex" justifyContent="space-between">
-                <Box display="flex" flexDirection="column">
-                  <span style={{ marginBottom: "10px", color: "#999999" }}>
-                    Customer Name:
-                  </span>
-                  <span style={{ marginBottom: "10px", color: "#999999" }}>
-                    Phone:
-                  </span>
-                  <span style={{ marginBottom: "10px", color: "#999999" }}>
-                    Appointment Time:
-                  </span>
-                  <span style={{ marginBottom: "10px", color: "#999999" }}>
-                    Service:
-                  </span>
-                  <span style={{ marginBottom: "10px", color: "#999999" }}>
-                    Diamond Quantity:
-                  </span>
-                </Box>
+              <Typography
+                align="center"
+                style={{
+                  fontWeight: "bold",
+                  color: "green",
+                  fontSize: 20,
+                }}
+              >
+                {"Successful Appointment"}
+              </Typography>
+            </DialogTitle>
+            <DialogContent>
+              <Box id="alert-dialog-description">
                 <Box
                   display="flex"
                   flexDirection="column"
-                  alignItems="flex-start"
-                  ml={2}
+                  justifyContent="center"
+                  align="center"
+                  marginBottom="10px"
                 >
-                  <span style={{ marginBottom: "10px" }}>
-                    {customer.lastName + " " + customer.firstName}
-                  </span>
-                  <span style={{ marginBottom: "10px" }}>{customer.phone}</span>
-                  <Typography
-                    style={{ fontWeight: "bold", marginBottom: "10px" }}
-                  >
-                    {appointmentTime}
+                  <Typography> Appointment details:</Typography>
+                  <Typography style={{ fontWeight: "bold", color: "#AE9A7F" }}>
+                    Code: #{valuationRequestId}
                   </Typography>
-                  <span style={{ marginBottom: "10px" }}>{service}</span>
-                  <span style={{ marginBottom: "10px" }}>
-                    {diamondQuantity}
-                  </span>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                  <Box display="flex" flexDirection="column">
+                    <span style={{ marginBottom: "10px", color: "#999999" }}>
+                      Customer Name:
+                    </span>
+                    <span style={{ marginBottom: "10px", color: "#999999" }}>
+                      Phone:
+                    </span>
+                    <span style={{ marginBottom: "10px", color: "#999999" }}>
+                      Appointment Time:
+                    </span>
+                    <span style={{ marginBottom: "10px", color: "#999999" }}>
+                      Service:
+                    </span>
+                    <span style={{ marginBottom: "10px", color: "#999999" }}>
+                      Diamond Quantity:
+                    </span>
+                  </Box>
+                  <Box
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="flex-start"
+                    ml={2}
+                  >
+                    <span style={{ marginBottom: "10px" }}>
+                      {customer.lastName + " " + customer.firstName}
+                    </span>
+                    <span style={{ marginBottom: "10px" }}>
+                      {customer.phone}
+                    </span>
+                    <Typography
+                      style={{ fontWeight: "bold", marginBottom: "10px" }}
+                    >
+                      {appointmentTime}
+                    </Typography>
+                    <span style={{ marginBottom: "10px" }}>{service}</span>
+                    <span style={{ marginBottom: "10px" }}>
+                      {diamondQuantity}
+                    </span>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-          </DialogContent>
-          <DialogActions>
-            <Box display="flex" justifyContent="center" width="100%">
-              <Button onClick={() => setOpen(false)} color="primary" autoFocus>
-                Close
-              </Button>
-            </Box>
-          </DialogActions>
-        </Dialog>
+            </DialogContent>
+            <DialogActions>
+              <Box display="flex" justifyContent="center" width="100%">
+                <Button
+                  onClick={() => setOpen(false)}
+                  color="primary"
+                  autoFocus
+                >
+                  Close
+                </Button>
+              </Box>
+            </DialogActions>
+          </Dialog>
+        </Box>
       </Box>
-    </Box>
-  );
+    );
+  };
 };
-
 export default AppointmentForm;
