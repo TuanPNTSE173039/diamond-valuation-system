0379320b74edfc12f2b0608bf2b0cddefe978ef2
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-07 17:36:03 +0700

    edit again origin button

--- src/components/Calculate/InputForm/Button/Origin.jsx
+++ src/components/Calculate/InputForm/Button/Origin.jsx
@@ -1,38 +1,38 @@
-import { Box, Button } from '@mui/material';
+import { Box, Button } from "@mui/material";
 
 // eslint-disable-next-line react/prop-types
-const OriginButton = ({ onClick, buttonName, icon, state, children, setState }) => {
-    return (
-        <Button
-            onClick={() => onClick(setState, buttonName)}
-            sx={{
-                width: '45%',
-                height: 37,
-                position: 'absolute',
-                top: 85,
-                boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-                borderRadius: '6px',
-                backgroundColor: state[buttonName] === 'clicked' ? '#4F46E5' : '#f5f5f5',
-                ':hover': { bgcolor: '#4F46E5', color: 'white' },
-                fontFamily: 'Inter-Regular, Helvetica',
-                color: state[buttonName] === 'clicked' ? 'white' : '#171a1f',
-                fontSize: 12,
-            }}
-        >
-            <Box
-                sx={{
-                    position: 'absolute',
-                    width: '5%',
-                    height: '20%',
-                    top: 8,
-                    left: 10,
-                }}
-            >
-                {icon}
-            </Box>
-            {children}
-        </Button>
-    );
+const OriginButton = ({ onClick, buttonName, icon, state, setState }) => {
+  return (
+    <Button
+      onClick={() => onClick(setState, buttonName)}
+      sx={{
+        width: 175,
+        height: 37,
+        position: "absolute",
+        boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+        borderRadius: "6px",
+        backgroundColor:
+          state[buttonName] === "clicked" ? "#4F46E5" : "#f5f5f5",
+        ":hover": { bgcolor: "#4F46E5", color: "white" },
+        fontFamily: "Inter-Regular, Helvetica",
+        color: state[buttonName] === "clicked" ? "white" : "#171a1f",
+        fontSize: 12,
+      }}
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          width: "5%",
+          height: "20%",
+          top: 8,
+          left: 10,
+        }}
+      >
+        {icon}
+      </Box>
+      {buttonName.toUpperCase()}
+    </Button>
+  );
 };
 
 export default OriginButton;
