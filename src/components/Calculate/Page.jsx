634d6d79957041772ee6f04ea5a8560203340fb6
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-07 22:40:10 +0700

    add useQuery to get value

--- src/components/Calculate/Page.jsx
+++ src/components/Calculate/Page.jsx
@@ -1,15 +1,58 @@
 import CalculateInputForm from "./InputForm/InputForm.jsx";
 import CalculateOutputForm from "./OutputForm.jsx";
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
+import { getDiamondPriceData } from "../../services/api";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 function CalculatePage() {
+  const [params, setParams] = useState({
+    diamondOrigin: "NATURAL",
+    caratWeight: "1.0",
+    color: "G",
+    clarity: "VS1",
+    cut: "EXCELLENT",
+    polish: "EXCELLENT",
+    symmetry: "EXCELLENT",
+    shape: "ROUND",
+    fluorescence: "NONE",
+  });
+
+  const [defaultData, setDefaultData] = useState({
+    fairPriceEstimate: 5115,
+    estimateRange: { min: 4135, max: 6193 },
+    last30DayChange: -2.72,
+  });
+
+  const [submitting, setSubmitting] = useState(false);
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["diamondPriceData", params],
+    queryFn: () => getDiamondPriceData(params),
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+  };
+
   return (
-    <Box style={{ display: "flex", justifyContent: "space-between", margin: "50px 0px"}}>
+    <Box
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        margin: "50px 0px",
+      }}
+    >
       <Box style={{ marginRight: "0px" }}>
-        <CalculateInputForm />
+        <CalculateInputForm setParams={setParams} handleSubmit={handleSubmit} />
       </Box>
       <Box>
-        <CalculateOutputForm />
+        <CalculateOutputForm
+          diamondData={data || defaultData}
+          isLoading={isLoading}
+          error={error}
+        />
       </Box>
     </Box>
   );
