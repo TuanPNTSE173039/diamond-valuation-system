7ef0aacee5d8f57533498ecb985f652b029871bc
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-10 11:47:49 +0700

    added background color of outlet

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,22 +1,20 @@
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./UI/Header.jsx";
 import Footer from "./UI/Footer.jsx";
 import CustomSeparator from "./UI/BreadCrumb.jsx";
 
 function RootLayout() {
-    return (
-        <div style={{display: "flex", flexDirection: "column"}}>
-            <Header/>
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Header />
 
-            <div style={{ paddingTop: '80px',
-                backgroundColor: "#f5f5f5",
-            }}>
-                <CustomSeparator/>
-                <Outlet/>
-            </div>
-            <Footer/>
-        </div>
-    );
+      <div style={{ paddingTop: "80px", backgroundColor: "#f5f5f5" }}>
+        <CustomSeparator />
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default RootLayout;
