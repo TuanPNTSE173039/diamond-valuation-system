8ec9e0d832e6746f52a8dd3e5624440b82f57d73
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-12 23:10:19 +0700

    add drawer ui

--- src/components/UI/Drawer.jsx
+++ src/components/UI/Drawer.jsx
@@ -1,123 +1,92 @@
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
-import MuiDrawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
+import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import { styled } from "@mui/material/styles";
+import AccountBoxOutlinedIcon from "@mui/icons-material/AccountBoxOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-//import * as React from "react";
-import { NavLink } from "react-router-dom";
-//import { checkRole } from "../../utilities/DrawerItemByRole.jsx";
+import { Container } from "@mui/material";
 
 const drawerWidth = 240;
 
-const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: "hidden",
-});
+function ResponsiveDrawer(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [isClosing, setIsClosing] = React.useState(false);
 
-const closedMixin = (theme) => ({
-    transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: "hidden",
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up("sm")]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
-});
+  const handleDrawerClose = () => {
+    setIsClosing(true);
+    setMobileOpen(false);
+  };
 
-export const DrawerHeader = styled("div")(({ theme }) => ({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-}));
+  const handleDrawerTransitionEnd = () => {
+    setIsClosing(false);
+  };
 
-const Drawer = styled(MuiDrawer, {
-    shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: "nowrap",
-    boxSizing: "border-box",
-    backgroundColor: "#333",
-    ...(open && {
-        ...openedMixin(theme),
-        "& .MuiDrawer-paper": openedMixin(theme),
-    }),
-    ...(!open && {
-        ...closedMixin(theme),
-        "& .MuiDrawer-paper": closedMixin(theme),
-    }),
-}));
+  const handleDrawerToggle = () => {
+    if (!isClosing) {
+      setMobileOpen(!mobileOpen);
+    }
+  };
 
-const UIDrawer = ({ theme, handleDrawerClose, ...props }) => {
-    return (
-        <Drawer variant="permanent" {...props}>
-            <DrawerHeader>
-                <IconButton onClick={handleDrawerClose}>
-                    {theme.direction === "rtl" ? (
-                        <ChevronRightIcon />
-                    ) : (
-                        <>
-                            <Typography variant="h6" noWrap component="div" sx={{ mr: 7 }}>
-                                Topics
-                            </Typography>
-                            <ChevronLeftIcon />
-                        </>
-                    )}
-                </IconButton>
-            </DrawerHeader>
-            <Divider />
-            <List>
-                {props.items.map((item) => {
-                    const link = item.link;
-                    return (
-                        <NavLink to={link} key={item.id}>
-                            <ListItem disablePadding sx={{ display: "block" }}>
-                                <ListItemButton
-                                    sx={{
-                                        minHeight: 48,
-                                        justifyContent: open ? "initial" : "center",
-                                        px: 2.5,
-                                    }}
-                                >
-                                    <ListItemIcon
-                                        sx={{
-                                            minWidth: 0,
-                                            mr: open ? 3 : "auto",
-                                            justifyContent: "center",
-                                        }}
-                                    >
-                                        {item.icon}
-                                    </ListItemIcon>
-                                    <ListItemText
-                                        primary={item.name}
-                                        sx={{
-                                            opacity: open ? 1 : 0,
-                                        }}
-                                    />
-                                </ListItemButton>
-                            </ListItem>
-                        </NavLink>
-                    );
-                })}
-            </List>
-        </Drawer>
-    );
-};
+  const drawer = (
+    <div>
+      <Toolbar>
+        <Typography variant="h6" noWrap component="div">
+          TOPIC
+        </Typography>
+      </Toolbar>
+      <Divider />
+      <List>
+        {["Profile", "Appointment"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? (
+                  <AccountCircleOutlinedIcon />
+                ) : (
+                  <AccountBoxOutlinedIcon />
+                )}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
+  );
 
-export default UIDrawer;
+  return (
+    <Box sx={{ display: "flex" }} aria-label="box">
+      <CssBaseline />
+      <Container aria-label=" folders">
+        <Box
+          component="nav"
+          sx={{ width: "250px", marginLeft: "30px" }}
+          aria-label="mailbox folders"
+        >
+          <div>{drawer}</div>
+        </Box>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+          }}
+        >
+          <Toolbar />
+        </Box>
+      </Container>
+    </Box>
+  );
+}
+
+export default ResponsiveDrawer;
