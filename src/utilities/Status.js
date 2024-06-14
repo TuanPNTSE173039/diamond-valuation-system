23881cf7f7f5e1b4f9ab5076b4492ae87df93cfd
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-15 00:20:02 +0700

    add more status

--- src/utilities/Status.js
+++ src/utilities/Status.js
@@ -1,16 +1,30 @@
-const valuationRequestStatus = [
-    {
-        id: 0,
-        name: "All requests",
-    },
-    {
-        id: 1,
-        name: "Pending",
-    },
-    {
-        id: 2,
-        name: "Processing",
-    },
+export const valuationRequestStatus = [
+  {
+    id: 0,
+    name: "ALL",
+  },
+  {
+    id: 1,
+    name: "PENDING",
+  },
+  {
+    id: 2,
+    name: "PROCESSING",
+  },
+  {
+    id: 3,
+    name: "RECEIVED",
+  },
+  {
+    id: 4,
+    name: "COMPLETED",
+  },
+  {
+    id: 5,
+    name: "SEALED",
+  },
+  {
+    id: 6,
+    name: "CANCELLED",
+  },
 ];
-
-export default valuationRequestStatus;
