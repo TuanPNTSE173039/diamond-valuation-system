4a59bdd9cb88b43b81e9284221e1a9193a32538f
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-15 00:19:47 +0700

    get table request

--- src/components/ManageAccount/List.jsx
+++ src/components/ManageAccount/List.jsx
@@ -1,7 +1,83 @@
-import Table from "../UI/Table";
+import React, { useState } from "react";
 import Drawer from "../UI/Drawer.jsx";
+import UITable from "../UI/Table";
+import { RequestHeadCells } from "../../utilities/Table.js";
+import { useQuery } from "@tanstack/react-query";
+import { getValuationRequests } from "../../services/api.js";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+// Utility function to format date and time
+const formatDateTime = (dateTimeString) => {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  return new Date(dateTimeString).toLocaleString(undefined, options);
+};
+
+// const [page, setPage] = useState(0);
+// const [rowsPerPage, setRowsPerPage] = useState(5);
 
 const ValuationRequestList = () => {
+  const {
+    data: requests,
+    isFetching: isRequestFetching,
+    error,
+  } = useQuery({
+    queryKey: ["requests"],
+    queryFn: () => getValuationRequests(2),
+  });
+
+  // Handle loading state
+  if (isRequestFetching) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  // Handle error state
+  if (error) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Typography variant="h6" color="error">
+          Failed to fetch data
+        </Typography>
+      </Box>
+    );
+  }
+
+  // Map data to the format expected by UITable
+  const requestRows = requests?.content.map((row) => {
+    return {
+      number: row.id,
+      status: row.status,
+      creationDate: formatDateTime(row.creationDate),
+      diamondAmount: row.diamondAmount,
+      service: row.serviceName,
+    };
+  });
+
   return (
     <div
       style={{
@@ -14,7 +90,11 @@ const ValuationRequestList = () => {
       <div style={{ marginRight: "20px" }}>
         <Drawer />
       </div>
-      <Table />
+      <UITable
+        heading="All Valuations"
+        rows={requestRows}
+        headCells={RequestHeadCells}
+      />
     </div>
   );
 };
