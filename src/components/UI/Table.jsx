b58f60ddb4f7d68ef0afe8ce6b5d7fa627e22dbd
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-15 00:21:06 +0700

    remove test data of table

--- src/components/UI/Table.jsx
+++ src/components/UI/Table.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -8,19 +8,7 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
-export default function BasicTable() {
+export default function UITable({ heading, rows = [], headCells = [] }) {
   return (
     <TableContainer component={Paper} sx={{ marginRight: "30px" }}>
       <Typography
@@ -29,31 +17,24 @@ export default function BasicTable() {
         component="div"
         sx={{ marginLeft: "30px", marginTop: "10px", fontWeight: "bold" }}
       >
-        Nutrition
+        {heading}
       </Typography>
       <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
         <TableHead>
           <TableRow>
-            <TableCell sx={{ fontWeight: "bold" }}>Dessert </TableCell>
-            <TableCell sx={{ fontWeight: "bold" }}>Calories</TableCell>
-            <TableCell sx={{ fontWeight: "bold" }}>Fat&nbsp;(g)</TableCell>
-            <TableCell sx={{ fontWeight: "bold" }}>Carbs&nbsp;(g)</TableCell>
-            <TableCell sx={{ fontWeight: "bold" }}>Protein&nbsp;(g)</TableCell>
+            {headCells.map((cell, index) => (
+              <TableCell sx={{ fontWeight: "bold" }} key={index}>
+                {cell.label}
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell>{row.calories}</TableCell>
-              <TableCell>{row.fat}</TableCell>
-              <TableCell>{row.carbs}</TableCell>
-              <TableCell>{row.protein}</TableCell>
+          {rows.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {Object.keys(row).map((key, cellIndex) => (
+                <TableCell key={cellIndex}>{row[key]}</TableCell>
+              ))}
             </TableRow>
           ))}
         </TableBody>
