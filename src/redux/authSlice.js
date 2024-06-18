fb220237bcc94ba4f23f45e79e2117d238482277
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-19 00:43:14 +0700

    add auth

---
+++ src/redux/authSlice.js
@@ -0,0 +1,80 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import AuthService from "../services/auth.service.js";
+import { setMessage } from "./messageSlide.js";
+
+const user = JSON.parse(localStorage.getItem("auth"))?.userInformation;
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({ username, email, password }, thunkAPI) => {
+    try {
+      const response = await AuthService.register(username, email, password);
+      thunkAPI.dispatch(setMessage(response.data.message));
+      return response.data;
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue();
+    }
+  },
+);
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ usernameOrEmail, password }, thunkAPI) => {
+    try {
+      const data = await AuthService.login(usernameOrEmail, password);
+      return { user: data.userInformation };
+    } catch (error) {
+      const message =
+        error?.response?.data?.message || error.message || error.toString();
+      thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue(message);
+    }
+  },
+);
+
+export const logout = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
+  try {
+    await AuthService.logout();
+    return null;
+  } catch (error) {
+    const message =
+      error?.response?.data?.message || error.message || error.toString();
+    thunkAPI.dispatch(setMessage(message));
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+const initialState = user
+  ? { isLoggedIn: true, user }
+  : { isLoggedIn: false, user: null };
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoggedIn = true;
+        state.user = action.payload.user;
+      })
+      .addCase(login.rejected, (state) => {
+        state.isLoggedIn = false;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.isLoggedIn = false;
+        state.user = null;
+      });
+  },
+});
+
+const { reducer } = authSlice;
+export default reducer;
