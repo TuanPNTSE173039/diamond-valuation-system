2dd7d533486a28e815ed71d9a9e2f1c22864bce1
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-19 00:38:25 +0700

    move poster img to left

--- src/components/HomePage/Poster.jsx
+++ src/components/HomePage/Poster.jsx
@@ -3,28 +3,27 @@ import diamond from "../../assets/images/diamond_poster.png";
 
 export default function Poster() {
   return (
+    <Box
+      sx={{
+        position: "relative",
+        height: "560px",
+        bgcolor: "#717171",
+        marginTop: "60px",
+        marginBottom: "30px",
+      }}
+    >
       <Box
-          sx={{
-            position: 'relative',
-            height: "560px",
-            bgcolor: '#717171',
-            marginTop: '60px',
-              marginBottom:'30px'
-          }}
-      >
-          <Box
-              component="img"
-              src={diamond}
-              alt="Diamond"
-              sx={{
-                  position: "absolute",
-                  width: "681px",
-                  height: "555px",
-                  top: "5px",
-                  left: "695px",
-              }}
-          />
-
-      </Box>
+        component="img"
+        src={diamond}
+        alt="Diamond"
+        sx={{
+          position: "absolute",
+          width: "681px",
+          height: "555px",
+          top: "5px",
+          left: "685px",
+        }}
+      />
+    </Box>
   );
 }
