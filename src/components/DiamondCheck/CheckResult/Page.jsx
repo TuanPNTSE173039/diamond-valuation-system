55febfa60bdd83b4fe342372e4fbf933fe3ab455
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-24 14:30:30 +0700

    add diamond details

--- src/components/DiamondCheck/CheckResult/Page.jsx
+++ src/components/DiamondCheck/CheckResult/Page.jsx
@@ -1,12 +1,13 @@
-import diamond_id from "../../../assets/images/diamond_id.png";
-import daniel from "../../../assets/images/daniel.png";
-import allurez from "../../../assets/images/allurez.png";
+import diamond1 from "../../../assets/images/diamond1.png";
+import dimond2 from "../../../assets/images/dimond2.png";
+import diamond3 from "../../../assets/images/diamond3.png";
 import DiamondCheckResultForm from "./ResultForm.jsx";
 import ImageGallery from "../CheckResult/ImageForm";
 import { Box } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getDiamondCheckByCertificateId } from "../../../services/api.js";
+import DiamondDetails from "./DiamondDetails.jsx";
 
 const DiamondCheckResultPage = () => {
   const { certificateID } = useParams();
@@ -24,52 +25,58 @@ const DiamondCheckResultPage = () => {
 
   const images = [
     {
-      img: diamond_id,
-      title: "Diamond ID Image", // replace with your actual title
-    },
-
-    {
-      img: daniel,
-      title: "Daniel Image", // replace with your actual title
+      img: diamond1,
+      title: "First Diamond Image",
     },
     {
-      img: allurez,
-      title: "Allurez Image", // replace with your actual title
+      img: dimond2,
+      title: "Second Diamond Image",
+    },
+    {
+      img: diamond3,
+      title: "Thirst Diamond Image",
     },
   ];
 
   return (
     <Box
       display="flex"
-      flexDirection="row"
+      flexDirection="column"
       marginY="50px"
       marginLeft="120px"
       justifyContent="space-between"
     >
-      <Box width="50%">
-        <ImageGallery images={images} />
+      <Box
+        display="flex"
+        flexDirection="row"
+        width="100%"
+        justifyContent="space-between"
+      >
+        <Box width="50%">
+          <ImageGallery images={images} />
+        </Box>
+        <Box width="50%" marginLeft="70px">
+          {diamondData && (
+            <DiamondCheckResultForm
+              giaId={diamondData.certificateId}
+              priceEstimate={`$${diamondData.fairPrice}`}
+              estimateRange={`$${diamondData.minPrice} - $${diamondData.maxPrice}`}
+              cut_score="0.9"
+              visual_carat="1.0"
+              shape={diamondData.shape}
+              carat={diamondData.caratWeight}
+              color={diamondData.color}
+              clarity={diamondData.clarity}
+              fluorescence={diamondData.fluorescence}
+              symmetry={diamondData.symmetry}
+              polish={diamondData.polish}
+              lab="GIA"
+            />
+          )}
+        </Box>
       </Box>
-      <Box width="50%" marginLeft="70px">
-        {diamondData && (
-          <DiamondCheckResultForm
-            giaId={diamondData.certificateId}
-            priceEstimate={`$${diamondData.fairPrice}`}
-            estimateRange={`$${diamondData.minPrice} - $${diamondData.maxPrice}`}
-            cut_score="0.9"
-            visual_carat="1.0"
-            //cut_score={cut_score}
-            //visual_carat={visual_carat}
-            shape={diamondData.shape}
-            carat={diamondData.caratWeight}
-            color={diamondData.color}
-            clarity={diamondData.clarity}
-            fluorescence={diamondData.fluorescence}
-            symmetry={diamondData.symmetry}
-            polish={diamondData.polish}
-            lab="GIA"
-            //lab={lab}
-          />
-        )}
+      <Box marginTop="40px">
+        <DiamondDetails />
       </Box>
     </Box>
   );
