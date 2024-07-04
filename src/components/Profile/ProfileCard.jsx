cace19be947d8c94e9797a0cb1de4cc6d98e4e00
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-07-04 16:03:46 +0700

    add toast message

--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -20,6 +20,8 @@ import {
 } from "../../services/api.js";
 import * as Yup from "yup";
 import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import { setMessage } from "../../redux/messageSlide.js";
 
 const styles = {
   details: {
@@ -39,6 +41,8 @@ const styles = {
 };
 
 export default function ProfileCard(props) {
+  const dispatch = useDispatch();
+  // const { message } = useSelector((state) => state.message);
   const [user, setUser] = useState({
     firstName: props.firstName,
     lastName: props.lastName,
@@ -51,15 +55,92 @@ export default function ProfileCard(props) {
     authID: props.authID,
   });
 
-  const validationSchema = Yup.object({
-    firstName: Yup.string().required("First name is required"),
-    lastName: Yup.string().required("Last name is required"),
-    phone: Yup.string().required("Phone number is required"),
-    address: Yup.string().required("Address is required"),
-    identityDocument: Yup.string().required("Identity document is required"),
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .matches(/^[A-Za-z]+$/, "The first name must be alphabetic.")
+      .test(
+        "len",
+        "The first name must be between 2 and 30 characters.",
+        (val) =>
+          val && val.toString().length >= 2 && val.toString().length <= 30,
+      )
+      .required("First name is required!"),
+    lastName: Yup.string()
+      .matches(/^[A-Za-z]+$/, "The last name must be alphabetic.")
+      .test(
+        "len",
+        "The last name must be between 2 and 30 characters.",
+        (val) =>
+          val && val.toString().length >= 2 && val.toString().length <= 30,
+      )
+      .required("Last name is required!"),
+    username: Yup.string()
+      .matches(/^[A-Za-z]+$/, "The username must be alphabetic.")
+      .test(
+        "len",
+        "The username must be between 6 and 24 characters.",
+        (val) =>
+          val && val.toString().length >= 6 && val.toString().length <= 24,
+      )
+      .required("Username is required!"),
     email: Yup.string()
-      .email("Invalid email format")
-      .required("Email is required"),
+      .email("This is not a valid email.")
+      .required("Email is required!"),
+    phone: Yup.string()
+      .test(
+        "len",
+        "The phone number must be 10 characters.",
+        (val) => val && val.toString().length === 10,
+      )
+      .test("is-numbers", "Phone number must only contain numbers.", (val) => {
+        return /^\d+$/.test(val);
+      })
+      .required("Phone number is required!"),
+    address: Yup.string()
+      .test(
+        "len",
+        "The address must be between 3 and 40 characters.",
+        (val) =>
+          val && val.toString().length >= 3 && val.toString().length <= 40,
+      )
+      .required("Address is required!"),
+    identityDocument: Yup.string()
+      .test(
+        "len",
+        "The identity document must be 12 characters.",
+        (val) => val && val.toString().length === 12,
+      )
+      .required("Identity document is required!"),
+    oldPassword: Yup.string()
+      .test(
+        "len",
+        "The password must be between 6 and 20 characters.",
+        (val) =>
+          val && val.toString().length >= 6 && val.toString().length <= 20,
+      )
+      .test(
+        "contains-number",
+        "The password must contain at least 1 number.",
+        (val) => {
+          return /\d/.test(val);
+        },
+      )
+      .required("Old password is required!"),
+    newPassword: Yup.string()
+      .test(
+        "len",
+        "The password must be between 6 and 20 characters.",
+        (val) =>
+          val && val.toString().length >= 6 && val.toString().length <= 20,
+      )
+      .test(
+        "contains-number",
+        "The password must contain at least 1 number.",
+        (val) => {
+          return /\d/.test(val);
+        },
+      )
+      .required("New password is required!"),
   });
 
   const [originalUser] = useState(user);
@@ -74,8 +155,8 @@ export default function ProfileCard(props) {
   const changeField = (event) => {
     const { name, value } = event.target;
     setUser({ ...user, [name]: value });
-    formik.setFieldValue(name, value); // Update Formik values
-    formik.setFieldTouched(name, true); // Mark field as touched
+    formik.setFieldValue(name, value);
+    formik.setFieldTouched(name, true);
   };
 
   const changePasswordField = (event) => {
@@ -123,12 +204,32 @@ export default function ProfileCard(props) {
 
   const handlePasswordUpdate = async () => {
     try {
-      await updateCustomerPassword(user.authID, passwords);
-      toast.success("Password updated successfully!");
-      handleClose();
+      const response = await updateCustomerPassword(user.authID, passwords);
+      if (response && response.data && response.data.message) {
+        const message = response.data.message;
+        dispatch(setMessage(message));
+        toast.error(message);
+      } else {
+        toast.success("Password updated successfully!");
+        handleClose();
+        setPasswords({ oldPassword: "", newPassword: "" });
+        formik.resetForm({
+          values: { ...formik.values, oldPassword: "", newPassword: "" },
+        });
+      }
     } catch (error) {
-      toast.error("Failed to update password.");
-      console.error("Failed to update password", error);
+      const message =
+        error?.response?.data?.message ||
+        error.message ||
+        "Failed to update password.";
+      dispatch(setMessage(message));
+
+      if (error?.response?.data?.message === "Incorrect old password") {
+        toast.error("Incorrect old password");
+      } else {
+        toast.error(message);
+        console.error("Failed to update password", error);
+      }
     }
   };
 
@@ -136,7 +237,7 @@ export default function ProfileCard(props) {
   const changeButton = async (event) => {
     event.preventDefault();
     if (!edit.disabled) {
-      await formik.handleSubmit();
+      await handleUpdate();
     }
     edit.disabled = !edit.disabled;
     edit.isEdit = !edit.isEdit;
@@ -151,10 +252,19 @@ export default function ProfileCard(props) {
       email: props.email || "",
       address: props.address || "",
       identityDocument: props.identityDocument || "",
+      oldPassword: "",
+      newPassword: "",
     },
+    validateOnBlur: true,
+    validateOnChange: false,
     validationSchema,
     onSubmit: handleUpdate,
   });
+
+  const handlePasswordChange = (event) => {
+    formik.handleChange(event); // Đảm bảo formik cập nhật giá trị
+    setPasswords({ ...passwords, [event.target.name]: event.target.value });
+  };
 
   return (
     <Card variant="outlined" sx={{ height: "730px", width: "96%" }}>
@@ -303,23 +413,42 @@ export default function ProfileCard(props) {
                 <DialogTitle>Change Password</DialogTitle>
                 <DialogContent>
                   <TextField
-                    autoFocus
+                    autoComplete="old-password"
                     margin="dense"
                     id="oldPassword"
                     name="oldPassword"
                     label="Old Password"
                     type="password"
                     fullWidth
-                    onChange={changePasswordField}
+                    onChange={handlePasswordChange}
+                    value={formik.values.oldPassword}
+                    onBlur={formik.handleBlur}
+                    error={
+                      formik.touched.oldPassword &&
+                      Boolean(formik.errors.oldPassword)
+                    }
+                    helperText={
+                      formik.touched.oldPassword && formik.errors.oldPassword
+                    }
                   />
                   <TextField
+                    autoComplete="new-password"
                     margin="dense"
                     id="newPassword"
                     name="newPassword"
                     label="New Password"
                     type="password"
                     fullWidth
-                    onChange={changePasswordField}
+                    onChange={handlePasswordChange}
+                    value={formik.values.newPassword}
+                    onBlur={formik.handleBlur}
+                    error={
+                      formik.touched.newPassword &&
+                      Boolean(formik.errors.newPassword)
+                    }
+                    helperText={
+                      formik.touched.newPassword && formik.errors.newPassword
+                    }
                   />
                 </DialogContent>
                 <DialogActions>
@@ -342,6 +471,7 @@ export default function ProfileCard(props) {
               size="large"
               variant="contained"
               color="secondary"
+              type="submit"
               onClick={changeButton}
             >
               {edit.isEdit === false ? "UPDATE" : "EDIT"}
