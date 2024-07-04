882533aed454241ffed5d5683499f123e78a810e
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-07-04 16:03:26 +0700

    add toast container

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient();
 
@@ -15,5 +17,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <App />
       </QueryClientProvider>
     </Provider>
+    <ToastContainer />
   </React.StrictMode>,
 );
