8332537058e110b0ff6be8965dcb53a3e1b93275
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-07-20 22:07:37 +0700

    change validate input value

--- src/components/DiamondCheck/CheckInput/Form.jsx
+++ src/components/DiamondCheck/CheckInput/Form.jsx
@@ -2,13 +2,13 @@ import { useNavigate } from "react-router-dom";
 import {
   Box,
   Button,
-  FormControlLabel, FormHelperText,
+  FormControlLabel,
+  FormHelperText,
   Radio,
   RadioGroup,
   TextField,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
 import checkImage from "../../../assets/images/check.jpg";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -16,7 +16,7 @@ import {
   getDiamondCheckByCertificateId,
   getValuationRequestDetails,
 } from "../../../services/api.js";
-import {useFormik} from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import { string } from "yup";
 
@@ -27,36 +27,23 @@ const DiamondCheckInputForm = () => {
     checkType: "certificateId",
   };
 
-  // const validationSchema = Yup.object(({
-  //   // inputValue: Yup.string().when("checkType", {
-  //   //   is: (formik) => formik.values.checkType === "certificateId",
-  //   //   then: () =>
-  //   //     string()
-  //   //       .required("Certificate ID is required")
-  //   //       .matches(/^[0-9]+$/, "Certificate ID must be numeric")
-  //   //       .length(10, "Certificate ID must be 10 characters."),
-  //   //   otherwise: Yup.string().matches(
-  //   //     /^[0-9]+$/,
-  //   //     "Valuation Code must be numeric",
-  //   //   ),
-  //   // }),
-  // });
   const validationSchema = Yup.object().shape({
     inputValue: Yup.string().when("checkType", {
       is: "certificateId",
-      then: () => string()
-        .required("Certificate ID is required")
-        .matches(/^[0-9]+$/, "Certificate ID must be numeric")
-        .length(10, "Certificate ID must be 10 characters."),
-      otherwise: () => string().required("Valuation Code is required").matches(
-        /^[0-9]+$/,
-        "Valuation Code must be numeric"
-      ),
-    })
-  })
+      then: () =>
+        string()
+          .required("Certificate ID is required")
+          .matches(/^[0-9]+$/, "Certificate ID must be numeric")
+          .length(10, "Certificate ID must be 10 characters."),
+      otherwise: () =>
+        string()
+          .required("Valuation Code is required")
+          .matches(/^[0-9]+$/, "Valuation Code must be numeric"),
+    }),
+  });
 
   const handleCheck = async (values) => {
-    const {inputValue, checkType} = values;
+    const { inputValue, checkType } = values;
     try {
       let response;
       if (checkType === "certificateId") {
@@ -75,19 +62,18 @@ const DiamondCheckInputForm = () => {
     } catch (error) {
       console.error("Error during form submission:", error); // Log the error
       if (error.response && error.response.status === 404) {
-        toast.info("Certificate ID or Valuation Code not found!");
+        toast.info("Certificate ID not found!");
       } else {
-        toast.error(error.message || "An unexpected error occurred.");
+        toast.info(error.message || "An unexpected error occurred.");
       }
     }
-
   };
   const formik = useFormik({
     initialValues,
     validationSchema,
     onSubmit: handleCheck,
-  })
-  console.log(formik.values)
+  });
+  console.log(formik.values);
   const CustomErrorMessage = ({ children }) => (
     <div
       style={{
@@ -162,100 +148,100 @@ const DiamondCheckInputForm = () => {
             and more for free
           </Typography>
 
-              <Box >
-                <RadioGroup
-                  row
-                  name={"checkType"}
-                  value={formik.values.checkType}
-                  onChange={formik.handleChange}
-                  sx={{ position: "relative", top: 20 }}
-                >
-                  <FormControlLabel
-                    value={"certificateId"}
-                    checked={
-                      formik.values.checkType === "certificateId"
-                    }
-                    control={<Radio />}
-                    label="Check by Certificate ID"
-                  />
-                  <FormControlLabel
-                    value={"valuationCode"}
-                    checked={formik.values.checkType === "valuationCode"}
-                    control={<Radio />}
-                    label="Check by Valuation Code"
-                  />
-                </RadioGroup>
+          <Box>
+            <RadioGroup
+              row
+              name={"checkType"}
+              value={formik.values.checkType}
+              onChange={formik.handleChange}
+              sx={{ position: "relative", top: 20 }}
+            >
+              <FormControlLabel
+                value={"certificateId"}
+                checked={formik.values.checkType === "certificateId"}
+                control={<Radio />}
+                label="Check by Certificate ID"
+              />
+              <FormControlLabel
+                value={"valuationCode"}
+                checked={formik.values.checkType === "valuationCode"}
+                control={<Radio />}
+                label="Check by Valuation Code"
+              />
+            </RadioGroup>
 
-                <Box
-                  position="relative"
-                  width="100%"
-                  height={50}
-                  top={30}
-                  left={0}
-                  bgcolor="white"
-                  borderRadius={1}
-                  border="1px solid #9095a0"
-                >
-                  <TextField
-                    name="inputValue"
-                    variant="standard"
-                    placeholder={
-                      formik.values.checkType === "certificateId"
-                        ? "Enter Certificate ID"
-                        : "Enter Valuation Code"
-                    }
-                    fullWidth
-                    InputProps={{
-                      disableUnderline: true,
-                      style: {
-                        paddingLeft: 20,
-                        paddingTop: 8,
-                        color: "black",
-                        fontSize: 16,
-                      },
-                    }}
-                    value={formik.values.inputValue}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    error={formik.touched.inputValue && Boolean(formik.errors.inputValue)}
-                  />
+            <Box
+              position="relative"
+              width="100%"
+              height={50}
+              top={30}
+              left={0}
+              bgcolor="white"
+              borderRadius={1}
+              border="1px solid #9095a0"
+            >
+              <TextField
+                name="inputValue"
+                variant="standard"
+                placeholder={
+                  formik.values.checkType === "certificateId"
+                    ? "Enter Certificate ID"
+                    : "Enter Valuation Code"
+                }
+                fullWidth
+                InputProps={{
+                  disableUnderline: true,
+                  style: {
+                    paddingLeft: 20,
+                    paddingTop: 8,
+                    color: "black",
+                    fontSize: 16,
+                  },
+                }}
+                value={formik.values.inputValue}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.inputValue && Boolean(formik.errors.inputValue)
+                }
+              />
 
-                  {formik.touched.inputValue && formik.errors.inputValue && (
-                    <FormHelperText
-                      error={true}
-                      sx={{
-                        color: "#ffffff", // Customize your helper text styles here
-                        fontSize: '0.75rem',
-                        paddingLeft: '20px',
-                        marginTop: '12px',
-                      }}
-                    >
-                      {formik.errors.inputValue}
-                    </FormHelperText>
-                  )}
-
-                </Box>
-
-                <Button
-                  type="submit"
+              {formik.touched.inputValue && formik.errors.inputValue && (
+                <FormHelperText
+                  error={true}
                   sx={{
-                    position: "relative",
-                    width: 151,
-                    height: 50,
-                    top: 50,
-                    left: "calc(50% - 75.5px)",
-                    backgroundColor: "#6366f2",
-                    borderRadius: 1,
-                    textTransform: "none",
-                    ":hover": { backgroundColor: "#6366f2" },
+                    color: "#ffffff", // Customize your helper text styles here
+                    fontSize: "0.75rem",
+                    paddingLeft: "20px",
+                    marginTop: "10px",
+                    textAlign: "end",
                   }}
-                  onClick={() => formik.submitForm()}
                 >
-                  <Typography color="white" fontSize={16} lineHeight="26px">
-                    Run free check
-                  </Typography>
-                </Button>
-              </Box>
+                  **{formik.errors.inputValue}
+                </FormHelperText>
+              )}
+            </Box>
+
+            <Button
+              type="submit"
+              sx={{
+                position: "relative",
+                width: 151,
+                height: 50,
+                top: 50,
+                left: "calc(50% - 75.5px)",
+                backgroundColor: "#6366f2",
+                borderRadius: 1,
+                textTransform: "none",
+                ":hover": { backgroundColor: "#6366f2" },
+              }}
+              onClick={() => formik.submitForm()}
+            >
+              <Typography color="white" fontSize={16} lineHeight="26px">
+                Run free check
+              </Typography>
+            </Button>
+          </Box>
         </Box>
       </Box>
     </Box>
